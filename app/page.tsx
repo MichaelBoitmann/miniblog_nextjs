17d16fd58aae2e8de92848112652993ff02fa482
Michael <michael.boitmann@gmail.com>
2024-07-10 22:56:04 +1200

    added api route

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 
-export default function Home() {
+export default async function HomePage() {
+  const response = await fetch("http://localhost:3000/api", {
+    next: { revalidate: 6000 }
+  });
+  const data = response.json();
+  console.log(data);
+
+
   return (
     <main className={styles.main}>
       Home
