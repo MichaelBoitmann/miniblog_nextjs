4656ca1d7cb70664ce15edbd5181e380f4d04cf8
Michael <michael.boitmann@gmail.com>
2024-07-17 22:39:30 +1200

    working on Post and page api

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,13 +1,35 @@
+"use client"
+
 import styles from "./SignUp.module.css"
+import Link from 'next/link'
 
 export default function SignUpPage() {
+  async function submitData(event: React.SyntheticEvent<HTMLFormElement>) {
+    const formData = new FormData(event.currentTarget)
+    const name = String(formData.get('name'))
+    const email = String(formData.get('email'))
+    if (name && email) {
+      await fetch('http://localhost:3000/api/signup', {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data)
+      })
+    }
+  }
+
+
+
+
   return (
-    <div>
-      <form action="">
+    <div className={styles.page}>
+      <form onSubmit={submitData}>
         <h1>Sign Up</h1>
         <input name="name" placeholder="Name" type="text" />
         <input name="email" placeholder="Email Address" type="email" />
         <button type="submit">Sign Up</button>
+        <Link className={styles.back} href="/">or Cancel</Link>
       </form>
     </div>
   )
