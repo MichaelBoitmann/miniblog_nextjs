656673245db59ad828fda2fa41def10e4026bbc1
Michael <michael.boitmann@gmail.com>
2024-07-23 00:13:54 +1200

    working on drafts page

--- app/drafts/page.tsx
+++ app/drafts/page.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export default function CreatePostPage() {
   return (
     <main>
@@ -5,6 +7,11 @@ export default function CreatePostPage() {
         <h1>
           Create Draft by Mr Boitmann
         </h1>
+        <input name="title" placeholder="Title" type="text" />
+        <input name="email" placeholder="Author (email address)" type="email" />
+        <textarea name="content" cols={50} rows={8} placeholder="Content" />
+        <button type="submit"></button>
+        <Link href="/">or Cancel</Link>
       </form>
     </main>
   )
