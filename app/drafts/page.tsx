c93e0e91fd8a79f12c06825f4163514c1187af73
Michael <michael.boitmann@gmail.com>
2024-07-22 23:27:28 +1200

    added create page

---
+++ app/drafts/page.tsx
@@ -0,0 +1,11 @@
+export default function CreatePostPage() {
+  return (
+    <main>
+      <form>
+        <h1>
+          Create Draft by Mr Boitmann
+        </h1>
+      </form>
+    </main>
+  )
+}
