0de71860ffc73a9c7b30779790c0cfb7bd7ea770
Michael <michael.boitmann@gmail.com>
2024-07-22 23:11:23 +1200

    working on create page

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,46 +1,31 @@
-import { redirect } from "next/navigation"
-import styles from "./SignUp.module.css"
-import Link from 'next/link'
-import prisma from "@/lib/prisma"
+import Link from "next/link";
+import styles from "./SignUp.module.css";
+import { redirect } from "next/navigation";
+import prisma from "@/lib/prisma";
 
-export default function SignUpPage() {
-
+export default function SignupPage() {
   async function submitAction(formData: FormData) {
-    "use server"
-    const name = String(formData.get('name'))
-    const email = String(formData.get('email'))
-    const data = {name, email}
+    "use server";
+    const name = String(formData.get("name"));
+    const email = String(formData.get("email"));
+    const data = { name, email };
     if (name && email) {
-      await prisma.user.create({ data })
-      redirect('/')
-    }  
+      await prisma.user.create({ data });
+      redirect("/");
+    }
   }
-  // async function submitData(event: React.SyntheticEvent<HTMLFormElement>) {
-  //   event.preventDefault()
-  //   const formData = new FormData(event.currentTarget)
-  //   const name = String(formData.get('name'))
-  //   const email = String(formData.get('email'))
-  //   const data = {name, email}
-  //   if (name && email) {
-  //     await fetch('http://localhost:3000/api/signup', {
-  //       method: "POST",
-  //       headers: {
-  //         "Content-Type": "application/json",
-  //       },
-  //       body: JSON.stringify(data),
-  //     })
-  //     router.push("/")
-  //   }
-  // }
+
   return (
     <div className={styles.page}>
       <form action={submitAction}>
         <h1>Sign Up</h1>
-        <input name="name" placeholder="Name" type="text" />
-        <input name="email" placeholder="Email Address" type="email" />
-        <button type="submit">Sign Up</button>
-        <Link className={styles.back} href="/">or Cancel</Link>
+        <input name="name" placeholder="Name" type="text" required />
+        <input name="email" placeholder="Email address" type="email" required />
+        <button type="submit">Sign up</button>
+        <Link className={styles.back} href="/">
+          or Cancel
+        </Link>
       </form>
     </div>
-  )
+  );
 }
