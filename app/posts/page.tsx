3c97c327b4dcc09021990ccfd4997d4ba67c08a1
Michael <michael.boitmann@gmail.com>
2024-07-25 22:23:19 +1200

    added post details

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -1,10 +1,25 @@
 interface Props {
-  
+  params: {
+    postId: string;
+  }
 }
 
 
 export default function PostPage({ params}) {
   console.log(params.postId)
+  await prima.post.fundUnique({
+    where: {
+      id: Number(params.postId)
+    },
+    include: {
+      author: true,
+    }
+  })
+
+  if (!post) {
+    notFound()
+  }
+
   return (
 
     <div>postpage</div>
